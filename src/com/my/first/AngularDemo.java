bb370d7d2af28b82510fe22476ea96b87a8bbdef
Avinash <avinashkashid1@gmail.com>
2016-04-17 23:29:56 +0530

    Add HTML and js file

--- src/com/my/first/AngularDemo.java
+++ src/com/my/first/AngularDemo.java
@@ -1,6 +1,8 @@
 package com.my.first;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +18,6 @@ public class AngularDemo extends HttpServlet {
        
     
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		PrintWriter writer = response.getWriter();
 	}
 }
